d99aa2564043319f9542d53acc6bf21cbce3b707
Aparna Pradeep <aparnapradeep31@gmail.com>
2024-03-17 05:20:54 +0530

    Page added

---
+++ src/pages/HomePage.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import Layout from '../components/Layout/Layout'
+
+const HomePage = () => {
+  return (
+   <Layout>
+    <h1>Home Page</h1>
+    </Layout>
+
+                                      
+      
+    
+  )
+}
+
+export default HomePage
