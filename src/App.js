791e993cc3f45e826f1ec4183ccbdb796f2e8915
Rosu J Edanad <rosujedanad@gmail.com>
2024-03-17 05:47:34 +0530

    added register js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
-import React from 'react'
+import {Routes, Route } from "react-router-dom";
+import HomePage from "./pages/HomePage";
+import Register from "./pages/Register";
 
 function App() {
   return (
-    <div className="container-fluid">
-      <h1 className="text-center">Expense Tracker</h1>
-    </div>
-  )
+    <>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/register" element={<Register />} />
+      </Routes>
+    </>
+  );
 }
 
 export default App
---
+++ src/pages/Register.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Register = () => {
+    return(
+        <>
+        <div className="d-flex align-items-center justify-content-center">
+            <h1>login page</h1>
+        </div>
+
+        </>
+    );
+}
+
+export default Register
