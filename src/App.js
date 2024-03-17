24913ff3571d897f3362cab79a9d6d4b56318739
Aparna Pradeep <aparnapradeep31@gmail.com>
2024-03-17 08:19:14 +0530

    error of antd

--- src/App.js
+++ src/App.js
@@ -1,15 +1,16 @@
+import React from "react";
 import {Routes, Route } from "react-router-dom";
 import HomePage from "./pages/HomePage";
 import Register from "./pages/Register";
 
 function App() {
   return (
-    <>
+    <div>
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/register" element={<Register />} />
       </Routes>
-    </>
+    </div>
   );
 }
 
--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -2,9 +2,6 @@ import React from 'react'
 import Footer from './Footer'
 import Header from './Header'
 
-
-
-
 const Layout = ({children}) => {
  return (
   <>
